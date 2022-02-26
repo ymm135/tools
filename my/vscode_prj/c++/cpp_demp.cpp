1a7d0d0c42a9186d688c33de4d077b55fff423a3
ymm <ymm@yangmingmingdeMacBook-Pro.local>
2022-02-26 17:09:05 +0800

    增加vscode的c/c++/java/go调试工程

---
+++ my/vscode_prj/c++/cpp_demp.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> vi = {1,2,3,4,5};
+    cout << "Hello C++! " << vi[0] << endl;
+    return 0;
+}
---
+++ my/vscode_prj/java/JavaDemo.java
@@ -0,0 +1,6 @@
+// java demo 
+public class JavaDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+    }
+}
